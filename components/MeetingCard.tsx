2a572852af22a04534c5f5bdaf741ac76851bb37
techy-savant <ogbanufavour@gmail.com>
2024-09-22 18:45:01 +0100

    optimizaton: codebase cleansing

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -41,22 +41,7 @@ const MeetingCard = ({
         </div>
       </article>
       <article className={cn("flex justify-start w-full relative", {})}>
-        {/* <div className="relative flex w-full max-sm:hidden">
-          {avatarImages.map((img, index) => (
-            <Image
-              key={index}
-              src={img}
-              alt="attendees"
-              width={40}
-              height={40}
-              className={cn("rounded-full", { absolute: index > 0 })}
-              style={{ top: 0, left: index * 28 }}
-            />
-          ))}
-          <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
-            +5
-          </div>
-        </div> */}
+ 
         {!isPreviousMeeting && (
           <div className="flex gap-2">
             <Button onClick={handleClick} className="rounded bg-green-1 px-6">
