9c603466a6f25c343b1b12156afcc011411d6740
techy-savant <ogbanufavour@gmail.com>
2024-09-26 16:13:00 +0100

    fix

--- components/LandingHome.tsx
+++ components/LandingHome.tsx
@@ -56,7 +56,10 @@ const LandingHome = () => {
       <div className=" grid grid-cols-2 lg:grid-cols-4 lg:gap-3 justify-around w-full">
         {HomeData.map(
           ({ title, Icon, mainContent, mobileContent }: HomeType) => (
-            <Card key={title} className="flex flex-col items-center m-4 max-sm:min-h-[80px] lg:w-full">
+            <Card
+              key={title}
+              className="flex flex-col items-center m-4 max-sm:min-h-[80px] lg:w-full"
+            >
               <CardHeader className="flex flex-row gap-2 justify-center items-center max-sm:p-2">
                 <Icon className="size-4 mt-2" />
 
@@ -75,57 +78,6 @@ const LandingHome = () => {
             </Card>
           )
         )}
-        {/* <Card className="flex flex-col items-center m-4 max-sm:h-fit">
-          <CardHeader className="flex flex-row gap-2 justify-center items-center max-sm:p-2">
-            <CalendarClock className="size-4 mt-2" />
-
-            <p className="text-sm sm:text-base font-medium leading-none max-sm:hidden">
-              Plan & Schedule Meets
-            </p>
-          </CardHeader>
-          <CardContent>
-            <p className="text-center max-sm:text-sm max-sm:hidden">
-              Schedule meetings with ease
-            </p>
-            <p className=" sm:hidden text-sm font-medium leading-none text-center">
-              Plan & Schedule Meets
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="flex flex-col items-center m-4  max-sm:min-h-[80px]">
-          <CardHeader className="flex flex-row gap-2 justify-center items-center max-sm:p-2">
-            <Video className="size-4 mt-2" />
-
-            <p className="text-sm sm:text-base font-medium leading-none max-sm:hidden">
-              Record Meetings
-            </p>
-          </CardHeader>
-          <CardContent>
-            <p className="text-center max-sm:text-sm max-sm:hidden">
-              Capture each meeting moment.
-            </p>
-            <p className=" sm:hidden text-sm font-medium leading-none text-center">
-              Record Meetings
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="flex flex-col items-center m-4  max-sm:min-h-[80px]">
-          <CardHeader className="flex flex-row gap-2 justify-center items-center max-sm:p-2">
-            <LineChart className="size-4 mt-2" />
-
-            <p className="text-sm sm:text-base font-medium leading-none max-sm:hidden">
-              Gather Meeting Insights
-            </p>
-          </CardHeader>
-          <CardContent>
-            <p className="text-center max-sm:text-sm max-sm:hidden w-full">
-              Track and analyze meeting data.
-            </p>
-            <p className=" sm:hidden text-sm font-medium leading-none text-center">
-              Gather Meeting Insights
-            </p>
-          </CardContent>
-        </Card> */}
       </div>
       <div className="w-full flex flex-wrap justify-center gap-2 m-4">
         <Button
